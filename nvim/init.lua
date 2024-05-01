4a11cb5db335b9dee2d0d503eeb2770299628b9e
Aaron Mikulka <aaronjmikulka@gmail.com>
2024-05-01 14:55:31 -0700

    blah blah blah, try to fix

--- nvim/init.lua
+++ nvim/init.lua
@@ -900,7 +900,6 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'gitcommit' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
