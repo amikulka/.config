91d45675c8bc9d60769ffc7f1d1490181306d355
Aaron Mikulka <aaronjmikulka@gmail.com>
2024-07-15 10:59:43 -0700

    adds jsx/tsx context crap.

--- nvim/lua/custom/plugins/comment.lua
+++ nvim/lua/custom/plugins/comment.lua
@@ -1,16 +1,22 @@
 return {
-  'numToStr/Comment.nvim',
+  { 'JoosepAlviste/nvim-ts-context-commentstring', opts = {} },
+  {
 
-  config = function()
-    vim.keymap.set('n', '<leader>/', function()
-      require('Comment.api').toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)
-    end, { desc = 'Toggle comment line' })
-    vim.keymap.set(
-      'v',
-      '<leader>/',
-      "<esc><cmd>lua require('Comment.api').toggle.blockwise(vim.fn.visualmode())<cr>",
-      { desc = 'Toggle comment for selection' }
-    )
-    require('Comment').setup()
-  end,
+    'numToStr/Comment.nvim',
+    config = function(opts, _)
+      vim.keymap.set('n', '<leader>/', function()
+        require('Comment.api').toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)
+      end, { desc = 'Toggle comment line' })
+      vim.keymap.set(
+        'v',
+        '<leader>/',
+        "<esc><cmd>lua require('Comment.api').toggle.blockwise(vim.fn.visualmode())<cr>",
+        { desc = 'Toggle comment for selection' }
+      )
+      opts.pre_hook = function()
+        return require('ts_context_commentstring.internal').calculate_commentstring()
+      end
+      require('Comment').setup(opts)
+    end,
+  },
 }
