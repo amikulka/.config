7bc142b96138e2f5d7d24f5e360fd6e057dad04a
Aaron Mikulka <aaronjmikulka@gmail.com>
2024-08-12 15:05:02 -0700

    remove bak

--- sketchybar.bak2/items/calendar.sh
+++
@@ -1,6 +0,0 @@
-sketchybar --add item calendar right \
-           --set calendar icon=􀧞 \
-             label="$(date +'%a %d %b %I:%M %p')" \
-             update_freq=10 \
-              script="$PLUGIN_DIR/calendar.sh"
-
