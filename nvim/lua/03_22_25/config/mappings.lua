5d70a7f496f89d213f65cdf54a797f033ab0739c
Aaron Mikulka <aaron.mikulka@january.com>
2025-03-22 11:15:32 -0700

    create new folder for refactor

---
+++ nvim/lua/03_22_25/config/mappings.lua
@@ -0,0 +1,55 @@
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = '[P]roject [V]iew' })
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv") -- move highlighted line(s) down
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv") -- move highlighted line(s) up
+
+vim.keymap.set('n', 'J', 'mzJ`z') -- join lines without moving cursor
+vim.keymap.set('n', '<C-d>', '<C-d>zz') -- move cursor down half a page and center it
+vim.keymap.set('n', '<C-u>', '<C-u>zz') -- move cursor up half a page and center it
+vim.keymap.set('n', 'G', 'Gzz') -- move to end of file and center it
+vim.keymap.set('n', 'n', 'nzzzv') -- move to next search result and center it
+vim.keymap.set('n', 'N', 'Nzzzv') -- move to previous search result and center it
+
+-- greatest remap ever
+vim.keymap.set('x', '<leader>p', [["_dP]], { desc = 'Paste without overwriting clipboard' })
+
+vim.keymap.set('i', '<C-c>', '<Esc>') -- exit insert mode with <C-c>
+
+vim.keymap.set('n', 'Q', '<nop>') -- disable Ex mode
+vim.keymap.set('n', '<leader>=', vim.lsp.buf.format, { desc = 'Format buffer' })
+
+-- replace word under cursor
+vim.keymap.set('n', '<leader>rw', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = '[R]eplace [W]ord' })
+-- Make file executable
+vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true, desc = 'Make file executable' })
+
+vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Write buffer' })
+vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = 'Quit buffer' })
+vim.keymap.set('n', '<leader>Q', ':q!<CR>', { desc = 'Force quit buffer' })
+
+-- Separate clipboard for yank and paste
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]], { desc = 'Yank to system clipboard' })
+vim.keymap.set({ 'n', 'v' }, '<M-c>', [["+y]], { desc = 'Copy to system clipboard' })
+vim.keymap.set('n', '<leader>Y', [["+Y]], { desc = 'Yank to system clipboard' })
+
+-- TODO: Move to copilot file
+-- vim.keymap.set('n', '<leader>tc', function()
+--   require('copilot.suggestion').toggle_auto_trigger()
+-- end, { desc = 'Toggle [C]opilot' })
+
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>') -- clear search highlights with <Esc>
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+
+-- TODO: Do I want this?
+-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+vim.api.nvim_set_keymap(
+  'n',
+  '<leader>taf',
+  '<cmd>lua vim.g.autoformat_enabled = not vim.g.autoformat_enabled; print("Autoformat is now " .. (vim.g.autoformat_enabled and "enabled" or "disabled"))<CR>',
+  { noremap = true, silent = true, desc = '[T]oggle Auto[F]ormat' }
+)
---
+++ nvim/lua/03_22_25/config/options.lua
@@ -0,0 +1,41 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '\\'
+
+vim.g.have_nerd_font = true -- Set to true if you have a Nerd Font installed
+
+vim.g.autoformat_enabled = true
+
+-- [[ Setting options ]]
+-- See `:help vim.opt`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
+
+vim.opt.cmdheight = 0 -- Set command line at bottom to not show
+vim.opt.number = true -- Show line numbers
+vim.opt.relativenumber = true -- Show relative line numbers
+vim.opt.mouse = 'a' -- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.showmode = false -- Don't show the mode, since it's already in status line
+vim.opt.breakindent = true -- Enable break indent
+vim.opt.undofile = true -- Save undo history
+vim.opt.ignorecase = true -- Case-insensitive searching
+vim.opt.smartcase = true -- Case-insensitive searching UNLESS \C or capital in search
+vim.opt.signcolumn = 'yes' -- Keep signcolumn on by default
+vim.opt.updatetime = 250 -- Decrease update time
+vim.opt.timeoutlen = 300 -- Displays which-key popup sooner
+vim.opt.splitright = true -- Configure how new verticle splits should be opened
+vim.opt.splitbelow = true -- Configure how new horizontal splits should be opened
+
+-- Sets how neovim will display certain whitespace in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+-- vim.opt.list = true
+-- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.opt.inccommand = 'split' -- Preview substitutions live, as you type!
+vim.opt.cursorline = true -- Show which line your cursor is on
+vim.opt.scrolloff = 20 -- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.fileencoding = 'utf-8'
+vim.opt.hlsearch = true -- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.wrap = false -- No wrap text
+vim.opt.expandtab = true -- Spaces for tabs
+vim.opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for
