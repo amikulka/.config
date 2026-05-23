a98a456a1a56beaa1d69d31c8df55d5fbc2300b7
Aaron Mikulka <aaron.mikulka@january.com>
2026-05-23 08:16:32 -0700

    set up avante better

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -1,9 +1,9 @@
 return {
   {
     'yetone/avante.nvim',
-    enabled = true,
     event = 'VeryLazy',
     version = false,
+    build = 'make',
     dependencies = {
       'nvim-treesitter/nvim-treesitter',
       'stevearc/dressing.nvim',
@@ -33,7 +33,6 @@ return {
     },
     opts = {
       provider = 'claude-code',
-      auto_suggestions_provider = 'claude',
       mode = 'agentic',
       acp_providers = {
         ['claude-code'] = {
@@ -41,37 +40,19 @@ return {
           args = { '-y', '-g', '@zed-industries/claude-code-acp' },
           env = {
             NODE_NO_WARNINGS = '1',
-            ANTHROPIC_API_KEY = os.getenv('ANTHROPIC_API_KEY'),
-            ANTHROPIC_BASE_URL = os.getenv('ANTHROPIC_BASE_URL'),
             ACP_PATH_TO_CLAUDE_CODE_EXECUTABLE = vim.fn.exepath('claude'),
-            ACP_PERMISSION_MODE = 'bypassPermissions',
+            ACP_PERMISSION_MODE = 'default',
           },
         },
-      },
-      providers = {
-        claude = {
-          endpoint = 'https://api.anthropic.com',
-          model = 'claude-sonnet-4-20250514',
-          api_key_name = 'AVANTE_ANTHROPIC_API_KEY',
-          timeout = 30000,
-          extra_request_body = {
-            temperature = 0.75,
-            max_tokens = 4096,
-          },
-        },
-      },
-      suggestion = {
-        debounce = 1500,
-        throttle = 1500,
       },
       behaviour = {
-        auto_suggestions = true,
+        auto_suggestions = false,
         auto_set_keymaps = true,
         auto_apply_diff_after_generation = false,
         minimize_diff = true,
         auto_add_current_file = true,
         enable_token_counting = true,
-        auto_approve_tool_permissions = true,
+        auto_approve_tool_permissions = false,
         acp_follow_agent_locations = true,
       },
       mappings = {
@@ -91,12 +72,6 @@ return {
         submit = {
           normal = '<CR>',
           insert = '<C-s>',
-        },
-        suggestion = {
-          accept = '<M-l>',
-          next = '<M-]>',
-          prev = '<M-[>',
-          dismiss = '<C-]>',
         },
         cancel = {
           normal = { '<C-c>', '<Esc>', 'q' },
@@ -128,9 +103,5 @@ return {
         },
       },
     },
-    build = 'make',
-    config = function(_, opts)
-      require('avante').setup(opts)
-    end,
   },
 }
